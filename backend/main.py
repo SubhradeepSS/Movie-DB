7dcf529898dcadb879a8104464c2cf63765617f0
Waryserpant122 <yashshah123boy@gmail.com>
2020-11-27 12:59:54 +0530

    [fix] bug where user is deleted

--- backend/main.py
+++ backend/main.py
@@ -61,6 +61,8 @@ def profile(user):
         cursor.execute(
             'SELECT name,email,contact FROM users WHERE username=%s', (user,))
         User = cursor.fetchone()
+        if User == None:
+            return redirect(url_for('login'))
         return render_template('profile.html', user=user, name=User[0], email=User[1], contact=User[2])
 
     password = request.form['password']
